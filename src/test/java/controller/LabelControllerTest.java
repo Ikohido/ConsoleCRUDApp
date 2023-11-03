8c637448953a33a461f833e13c4f37f31feebea0
ikohido <rus.kanpaev@mail.ru>
2023-11-03 18:39:23 +0300

    Created PostController tests

--- src/test/java/controller/LabelControllerTest.java
+++ src/test/java/controller/LabelControllerTest.java
@@ -2,23 +2,17 @@ package controller;
 
 import model.Label;
 import model.PostStatus;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import repository.LabelRepository;
 import view.LabelView;
 
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 public class LabelControllerTest {
     @Mock
@@ -30,29 +24,72 @@ public class LabelControllerTest {
     @Mock
     Label label = new Label(1, "Тест1", PostStatus.ACTIVE);
 
+    /*      Данный метод создает метку и сохраняет ее в labels.json
+            Create: Создание метки
+            Save: Сохранение метки в json файл
+            Equals: Сравнение ожидаемой и действительной меток.
+     */
     @Test
     public void createLabelTest() {
+        //        --- Create ---
         labelController.createLabel(label.getId(), label.getName());
+        //        --- Save ---
         labelRepository.save(label);
-        assertEquals(label, labelRepository.getById(label.getId()));
-    }
-    @Test
-    public void editLabelTest() {
-        label.setName("Тест2");
-        labelRepository.update(label);
+        //        --- Equals ---
         assertEquals(label, labelRepository.getById(label.getId()));
     }
 
+    /*      Данный метод изменяет название метки
+            Edit: Изменение
+            Update: Сохранение изменения
+            Equals: Сравнение ожидаемой и действительной меток.
+    */
     @Test
-    public void deleteLabelTest() {
-        label.setStatus(PostStatus.DELETED);
+    public void editLabelTest() {
+        //        --- Edit ---
+        label.setName("Тест2");
+        //        --- Update ---
         labelRepository.update(label);
+        //        --- Equals ---
         assertEquals(label, labelRepository.getById(label.getId()));
     }
+
+    /*      Данный метод изменяет статус метки на удаленный (DELETED)
+            NewStatus: Изменение статуса на удаленный
+            UpdateStatus: Сохранение нового статуса
+            Equals: Сравнение ожидаемой и действительной меток.
+    */
     @Test
-    public void getLabelTest(){
-        Label label2 = labelRepository.getById(label.getId());
-        assertEquals(label, label2);
+    public void deleteLabelTest() {
+        //        --- NewStatus ---
+        label.setStatus(PostStatus.DELETED);
+        //        --- UpdateStatus ---
+        labelRepository.update(label);
+        //        --- Equals ---
+        assertEquals(label, labelRepository.getById(label.getId()));
+    }
+
+    /*  Данный метод выводит данные о указанной по ID метке.
+        Output: Захват вывода в консоль
+        SetOutput: Использование захвата и вывод данных в output
+        ShowLabel: Использование метода showLabel() для выдачи данных о метке.
+        SetOut: Захват вывода прерывается.
+        Expect: Что мы ожидаем увидеть от showLabel()
+        Equals: Сравнение ожидаемой и действительной меток.*/
+    @Test
+    public void getLabelTest() {
+        //        --- Output ---
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //        --- SetOutput ---
+        System.setOut(new PrintStream(outputStream));
+        //        --- ShowLabel ---
+        labelView.showLabel(label);
+        //        --- SetOut ---
+        System.setOut(System.out);
+        //        --- Expect ---
+        String expectedOutput = "Label Details:\r\nID: " + label.getId() + "\r\nName: " + label.getName() + "\r\n";
+        //        --- Equals ---
+        assertEquals(expectedOutput, outputStream.toString());
     }
 
 }
